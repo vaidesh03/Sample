ee6e92b9df67e191f26f20f636cdf032914b9347
Vaidesh03 <vaidesh03@gmail.com>
2021-11-12 10:43:22 +0530

    Added JS practice Function
    Age calcuation for retirement

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-let firstName = "Vaibhav Deshmukh";
-console.log(firstName);
+const calcAge = function (birthYear){
+    return 2037 - birthYear;
+}
+const yearUntilRetirement = function (birthYear, firstName){
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+    return retirement;
+}
+
+console.log(yearUntilRetirement(1991, 'Vaibhav Deshmukh'));
+console.log(yearUntilRetirement(1980, 'Laxman Deshmukh'));
